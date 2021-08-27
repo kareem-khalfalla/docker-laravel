2c91476f9b8334a6f97338a66f8f19ce8a904588
kareem-khalfalla <kareem.khalfalla.dev@gmail.com>
2021-08-27 04:42:18 +0200

    Add app root dir

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+
+phpinfo();
+
+/**
+ * Now you can interact within your docker php container,
+ * docker exec -it php bash
+ * `composer create-project laravel/laravel --prefer-dist .`
+ * This command assume the dir is empty, However,
+ * You can remove me and try run it :) happy hacking!
+ */
